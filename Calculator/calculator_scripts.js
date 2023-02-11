4c7c11191e9a14728762e9806c9a130d011a8f98
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-02-11 12:15:03 -0500

    index_page.html

--- Calculator/calculator_scripts.js
+++ Calculator/calculator_scripts.js
@@ -1,3 +1,9 @@
-function form1{
+function doMath () {
+  let form1 = document.form1.answer.value;
+  let button = Math.sqrt(form1);
+  document.form1.answer.value = button;
+}
 
+function doMath2 () {
+  let form1 = document.form1.answer.value;
 }
