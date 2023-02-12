40f91fab4ea62cef64d40ebf08039f45a5ed30b0
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-02-11 19:02:03 -0500

    index_page.html

--- Calculator/calculator_scripts.js
+++ Calculator/calculator_scripts.js
@@ -6,4 +6,12 @@ function doMath () {
 
 function doMath2 () {
   let form1 = document.form1.answer.value;
+  let button = (form1/100);
+  document.form1.answer.value = button;
+}
+
+function sign(){
+  let form1 = document.form1.answer.value;
+  let button = (form1 * -1);
+  document.form1.answer.value = button;
 }
